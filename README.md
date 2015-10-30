e67bcceb828ec1462b779af3344ca3e0446798d9
vbanas <val.banas@gmail.com>
2015-10-30 23:34:23 +0200

    ASDF 3 for smug. Smug dependency added.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Required:
 - SBCL
 - Quicklisp: /home/quicklisp/setup.lisp
+- ASDF 3 (required for Smug)
+cd ~/quicklisp/local-projects/
+git clone https://gitlab.common-lisp.net/asdf/asdf.git
+cd asdf
+make
+cd ..
+git clone http://github.com/fare/lisp-interface-library/
+- Smug 
+cd ~/quicklisp/local-projects
+git clone https://github.com/drewc/smug.git
--- init.lisp
+++ init.lisp
@@ -6,8 +6,16 @@
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
 
+;; smug requires new ASDF 3
+(let ((asdf:*central-registry* 
+       (list (merge-pathnames "quicklisp/local-projects/asdf/"
+                              (user-homedir-pathname)))))
+  (asdf:upgrade-asdf) (ql:quickload :asdf))
+
+(asdf:asdf-version)
+
 ;; quickload dependencies
-(ql:quickload "asdf")
+(ql:quickload :smug)
 
 ;;; optimization options
 (proclaim '(optimize (debug 3) (safety 3)))
